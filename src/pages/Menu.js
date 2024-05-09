26895909ca5a5012033ae8373ce0c8ad7c6bf62d
adriamariavs <125682807+adriamariavs@users.noreply.github.com>
2024-05-09 18:31:14 -0300

    09

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -74,8 +74,8 @@ function CarouselCardItem({ item }) {
             padding: 15,
           }}
         >
-          <Text style={{ fontSize: 20, fontWeight: 700 }}>R$ 20,00</Text>
-          <Text style={{ fontSize: 20, fontWeight: 700 }}>Cappuccino</Text>
+          <Text style={{ fontSize: 20, fontWeight: "700" }}>R$ 20,00</Text>
+          <Text style={{ fontSize: 20, fontWeight: "700" }}>Cappuccino</Text>
           <Text style={{ color: "gray" }}>
             O cappuccino, com sua mistura irresistível de café expresso, leite
             vaporizado e espuma de leite cremosa, é uma verdadeira obra-prima na
@@ -103,65 +103,73 @@ function CarouselCardItem({ item }) {
 export default function Menu() {
   const navigation = useNavigation();
 
-  const [heightValue] = useState(new Animated.Value(0));
-
-  // Função para iniciar a animação quando o componente é montado
-  useEffect(() => {
-    Animated.timing(heightValue, {
-      toValue: 80, // Defina o valor final da altura aqui
-      duration: 5000,
-      useNativeDriver: false, // `useNativeDriver` não é suportado para animações de layout
-    }).start();
-  }, []);
+  const [selectedButton, setSelectedButton] = useState("hotCoffee");
 
   return (
     <View style={{ flex: 1, backgroundColor: "#FFF" }}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <Animated.View
-          style={{
-            backgroundColor: "#d4a57b",
-            height: heightValue,
-            borderBottomLeftRadius: 100,
-          }}
-        />
-        <View
-          style={{
-            flexDirection: "row",
-            height: 70,
-            alignItems: "center",
-            justifyContent: "space-around",
-          }}
-        >
+        <View style={{ flexDirection: "row", height: 70, alignItems: "center", justifyContent: "space-around" }}>
+          {/* Botão Hot Coffee */}
           <TouchableOpacity
             style={{
               padding: 5,
               justifyContent: "center",
               alignItems: "center",
+              backgroundColor: selectedButton === "hotCoffee" ? "#d4a57b" : "#FFF",
             }}
+            onPress={() => setSelectedButton("hotCoffee")}
           >
-            <Text style={{ color: "#d4a57b" }}>Hot Coffee</Text>
+            <Text style={{ color: selectedButton === "hotCoffee" ? "#FFF" : "#d4a57b" }}>Hot Coffee</Text>
             <View
               style={{
-                borderBottomWidth: 2,
+                borderBottomWidth: selectedButton === "hotCoffee" ? 2 : 0,
                 borderColor: "#d4a57b",
                 width: 50,
               }}
-            ></View>
+            />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Text>Cold Coffee</Text>
+
+          {/* Botão Cold Coffee */}
+          <TouchableOpacity
+            style={{
+              padding: 5,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: selectedButton === "coldCoffee" ? "#d4a57b" : "#FFF",
+            }}
+            onPress={() => setSelectedButton("coldCoffee")}
+          >
+            <Text style={{ color: selectedButton === "coldCoffee" ? "#FFF" : "#d4a57b" }}>Cold Coffee</Text>
+            <View
+              style={{
+                borderBottomWidth: selectedButton === "coldCoffee" ? 2 : 0,
+                borderColor: "#d4a57b",
+                width: 50,
+              }}
+            />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Text>Cookies</Text>
+
+          {/* Botão Cookies */}
+          <TouchableOpacity
+            style={{
+              padding: 5,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: selectedButton === "cookies" ? "#d4a57b" : "#FFF",
+            }}
+            onPress={() => setSelectedButton("cookies")}
+          >
+            <Text style={{ color: selectedButton === "cookies" ? "#FFF" : "#d4a57b" }}>Cookies</Text>
+            <View
+              style={{
+                borderBottomWidth: selectedButton === "cookies" ? 2 : 0,
+                borderColor: "#d4a57b",
+                width: 50,
+              }}
+            />
           </TouchableOpacity>
         </View>
-        <View
-          style={{
-            height: 400,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
+        <View style={{ height: 400, justifyContent: "center", alignItems: "center" }}>
           <Carousel
             data={data}
             renderItem={CarouselCardItem}
@@ -174,10 +182,9 @@ export default function Menu() {
             layoutCardOffset={`9`}
           />
         </View>
-        <View
-          style={{ height: 300, width: "100%", backgroundColor: "white" }}
-        ></View>
+        <View style={{ height: 300, width: "100%", backgroundColor: "white" }}></View>
       </ScrollView>
     </View>
   );
 }
+
